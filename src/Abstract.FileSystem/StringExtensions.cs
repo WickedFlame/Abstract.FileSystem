fcb78fd051a21b6d55dfc1ce9b5fc9c351b1f7f4
Christian Walpen <christianwalpen@opacc.ch>
2023-12-22 10:27:42 +0100

    Added string Extensions for RemoveLeadingSlash and RemoveTrailingSlash

---
+++ src/Abstract.FileSystem/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract.FileSystem
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Remove a leading slash
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string RemoveLeadingSlash(this string value)
+        {
+            if (value.StartsWith("/") || value.StartsWith("\\"))
+            {
+                return value.Substring(1);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Remove a trailing slash
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string RemoveTrailingSlash(this string value)
+        {
+            if (value.EndsWith("/") || value.EndsWith("\\"))
+            {
+                return value.Substring(0, value.Length - 1);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ src/Tests/Abstract.FileSystem.Test/StringExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.FileSystem.Test
+{
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void StringExtensions_RemoveLeadingSlash_Slash()
+        {
+            "/test/leading/".RemoveLeadingSlash().Should().Be("test/leading/");
+        }
+
+        [Test]
+        public void StringExtensions_RemoveLeadingSlash_Backslash()
+        {
+            "\\test\\leading\\".RemoveLeadingSlash().Should().Be("test\\leading\\");
+        }
+
+        [Test]
+        public void StringExtensions_RemoveTrailingSlash_Slash()
+        {
+            "/test/leading/".RemoveTrailingSlash().Should().Be("/test/leading");
+        }
+
+        [Test]
+        public void StringExtensions_RemoveTrailingSlash_Backslash()
+        {
+            "\\test\\leading\\".RemoveTrailingSlash().Should().Be("\\test\\leading");
+        }
+    }
+}
