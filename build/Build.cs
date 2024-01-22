c5840e50402956e51553e7d0e13f9d728c77e13a
Christian Walpen <christianwalpen@opacc.ch>
2024-01-22 09:17:01 +0100

    Prepare next version

--- build/Build.cs
+++ build/Build.cs
@@ -32,7 +32,7 @@ class Build : NukeBuild
     [GitRepository] readonly GitRepository GitRepository;
 
     [Parameter("Version to be injected in the Build")]
-    public string Version { get; set; } = $"1.0.3";
+    public string Version { get; set; } = $"1.0.4";
 
     [Parameter("The Buildnumber provided by the CI")]
     public int BuildNo = 1;
